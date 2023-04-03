f59cfb7ef557645966a895a695e023cbe72dbe6f
TorriEetu <eetu.torri33@gmail.com>
2023-04-03 17:21:13 +0300

    added configureStore

--- osa6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -28,7 +28,6 @@ export const initializeAnecdotes = () => {
     dispatch(setAnecdotes(anecdotes))
   }
 }
-
 export const voteAnecdote = (anecdote) => {
   return async (dispatch) => {
     const votedAnecdote = await anecdoteService.update(anecdote)
--- osa6/redux-anecdotes-main/src/store.js
+++ osa6/redux-anecdotes-main/src/store.js
@@ -1,18 +1,16 @@
-import { createStore, combineReducers, applyMiddleware  } from 'redux'
+import { applyMiddleware  } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 import thunk from 'redux-thunk'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-  notification: notificationReducer,
-})
-
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk)
-)
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+    notification: notificationReducer,
+  }
+}, applyMiddleware(thunk))
 
 export default store
